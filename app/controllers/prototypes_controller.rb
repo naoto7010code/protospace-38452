2c6f846f1929ffad13a739dce6bfcdb19f3e38e1
naoto7010code <n.m.1210@au.com>
2022-11-13 20:32:03 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,60 @@
+class PrototypesController < ApplicationController
+  before_action :move_to_index, except: [:index, :show, :edit, :destroy]
+  before_action :authenticate_user!, except: [:index, :show, :edit, :destroy]
+
+  def index
+    @prototypes = Prototype.all.includes(:user)
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    # binding.pry
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path(@prototype)
+    else
+      @prototype = @prototype.includes(:user)
+      render :new
+    end
+  end
+
+  def show
+    @prototype = Prototype.find(params[:prototype_id] || params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @prototype = Prototype.find(params[:id])
+    @prototype.destroy
+    redirect_to root_path
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: [:index, :edit]
+    end
+  end
+
+end
